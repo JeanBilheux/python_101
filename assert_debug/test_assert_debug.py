20fe0280b2abba4e487acc7b64e17f91fde48505
JeanBilheux <bilheuxjm@ornl.gov>
2015-12-18 21:36:45 -0500

    Continued work on list comprehension

--- assert_debug/test_assert_debug.py
+++ assert_debug/test_assert_debug.py
@@ -1,1 +1,13 @@
 # testing assert and debug statement
+
+if __debug__:
+    print("we are in debug mode")
+
+a = 0
+assert a == 0 
+    
+print("outside debug!")
+
+
+#python -O test_assert_debug.py
+# to turn off the debugging mode
