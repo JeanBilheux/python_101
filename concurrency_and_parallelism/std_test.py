35ca846b3c93b217d14a0c89b0ffcf5b4334eec8
JeanBilheux <bilheuxjm@ornl.gov>
2016-01-21 15:46:44 -0500

    Started work on parallelism

---
+++ concurrency_and_parallelism/std_test.py
@@ -0,0 +1,3 @@
+import sys
+sys.stdout.write("Testing message to stdout\n")
+sys.stderr.write("Testing messgae to stderr\n")
---
+++ concurrency_and_parallelism/write_to_stdin.py
@@ -0,0 +1,3 @@
+import sys
+input = sys.stdin.read()
+sys.stdout.write("Received: %s" %input)
