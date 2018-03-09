724b24a18367467fa230f8b61904eefbd143fc34
JeanBilheux <bilheuxjm@ornl.gov>
2018-03-09 10:31:02 -0500

    Working on finding neunorm slow bug

---
+++ parallelization/python_code/parallel_processing_loading_images.py
@@ -0,0 +1,41 @@
+import argparse
+import glob
+from PIL import Image
+import time
+import numpy as np
+
+from NeuNorm.normalization import Normalization
+
+parser = argparse.ArgumentParser(description='Loading tiff using parallel processing')
+parser.add_argument('-f', '--files', help='list of files to load')
+parser.add_argument('--algo', help='algo to use to load data')
+
+def run_processing():
+
+    args = parser.parse_args()
+    list_of_files = glob.glob(args.files)
+
+    print(f"Loading {len(list_of_files)} files")
+    data = []
+
+    if args.algo == 'pillow':
+
+        for _file in list_of_files:
+            _image = Image.open(_file)
+            _data = np.asarray(_image)
+            data.append(_data)
+
+    elif args.algo == 'neunorm':
+
+        o_norm = Normalization()
+        o_norm.load(file=list_of_files, gamma_filter=False)
+        data = o_norm.data['sample']['data']
+
+    print(np.shape(data))
+
+if __name__ == "__main__":
+    start_time = time.time()
+    run_processing()
+    end_time = time.time()
+
+    print(f"It took {end_time - start_time}s")
