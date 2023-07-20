dac435dae0c8f42b6caf178dccdbad509e21003b
JeanBilheux <bilheuxjm@ornl.gov>
2023-07-20 13:57:51 -0400

    exporting data at the end of script

--- testing_algorithms/pystackreg_script.py
+++ testing_algorithms/pystackreg_script.py
@@ -3,6 +3,9 @@ import os
 import numpy as np
 from pystackreg import StackReg
 from skimage import io
+from tqdm import tqdm
+from PIL import Image
+
 
 input_folder = "/HFIR/CG1D/IPTS-30750/shared/processed_data/normalized/cropped/23_06_09_left/"
 assert os.path.exists(input_folder)
@@ -22,3 +25,19 @@ sr = StackReg(StackReg.RIGID_BODY)
 out_first = sr.register_transform_stack(list_images_3d_array, reference='first')
 
 print("Done !")
+
+print("Exporting")
+
+input_base_folder_name = os.path.basename(input_folder)
+input_folder_name = os.path.dirname(input_folder)
+
+output_folder_name = os.path.join(input_folder_name, input_base_folder_name + "_registered")
+
+for _index in tqdm(np.arange(len(list_files))):
+    filename = list_files[_index]
+    base_name = os.path.basename(filename)
+    output_filename = os.path.join(output_folder_name, base_name)
+    im = Image.fromarray(out_first[_index])
+    im.save(output_filename)
+
+print("Done exporting!")
